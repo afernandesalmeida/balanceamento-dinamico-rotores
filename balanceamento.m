ab93c2ec9c9bf4dfed7d270fad8dde1bbea5f4fd
Andre Almeida <afernandesalmeida@outlook.com>
2015-10-19 09:05:17 -0200

    adicionando conversores

--- balanceamento.m
+++ balanceamento.m
@@ -18,3 +18,10 @@ m_A=100; m_C=150; m_E=50; %massas conhecidas
 r_A=250; r_B=250; r_C=250; r_D=250; r_E=250; %raios conhecidos
 z_A=200; z_B=0; z_C=200; z_D=700; z_E=1000; %distancia em z
 teta_A=30; teta_C=270; teta_E=150; %angulos conhecidos em graus
+
+%Conversão de graus para radianos para que possa ser utilizado nas
+%funçoes sin() e cos()
+
+t_A=degtorad(teta_A);
+t_C=degtorad(teta_C);
+t_E=degtorad(teta_E);

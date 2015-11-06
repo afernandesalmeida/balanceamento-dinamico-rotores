9ec2c807235091d5bb3ea164a7b17abcf44b740d
Andre Almeida <afernandesalmeida@outlook.com>
2015-11-06 11:21:32 -0200

    primeira parte

--- balanceamento.m
+++ balanceamento.m
@@ -25,3 +25,17 @@ teta_A=30; teta_C=270; teta_E=150; %angulos conhecidos em graus
 t_A=degtorad(teta_A);
 t_C=degtorad(teta_C);
 t_E=degtorad(teta_E);
+
+% Calculo de teta D
+%(Eq. desenvolvida a partir do somatorio de Momentos em B)
+t_D=atan2((m_A*r_A*sin(t_A)*z_A - m_C*r_C*sin(t_C)*z_C - m_E*r_E*sin(t_E)*z_E),(m_A*r_A*cos(t_A)*z_A - m_C*r_C*cos(t_C)*z_C - m_E*r_E*cos(t_E)*z_E));
+
+% Conversão de teta D de radianos para graus tornando de fácil entendimento
+%para o usuário
+teta_D =radtodeg(t_D)
+
+% Calculo de m_D*r_D
+mr_D = ((((m_A*r_A*sin(t_A)*z_A - m_C*r_C*sin(t_C)*z_C - m_E*r_E*sin(t_E)*z_E)^2) + ((m_A*r_A*cos(t_A)*z_A - m_C*r_C*cos(t_C)*z_C - m_E*r_E*cos(t_E)*z_E)^2))^(1/2))*(1/z_D);
+
+% Calculo da massa de D
+m_D=mr_D/r_D
